a3f44474d32d5454638eee2789f8c6b15397124d
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-07-31 16:05:55 -0600

    Update to Django Checkbook Project
    Final code for Django Checkbook Project

--- Django_Checkbook_Project/BlueBirdBanking/Checkbook/urls.py
+++ Django_Checkbook_Project/BlueBirdBanking/Checkbook/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     # Sets the url path to the home page index.html
@@ -7,7 +7,7 @@ urlpatterns = [
     # Sets the url path to Create New Account page CreateNewAccount.html
     path('create/', views.create_account, name='create'),
     # Sets the url path to Balance Sheet page BalanceSheer.html
-    path('balance/', views.balance, name='balance'),
+    path('<int:pk>/balance/', views.balance, name='balance'),
     # Sets the url path to the Add New Transaction page AddNewTransaction.html
     path('transaction/', views.transaction, name='transaction'),
 ]
